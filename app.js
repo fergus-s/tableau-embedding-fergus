fca697e88f77eafb9df26635bd364cb87f080884
fergus-s <f.a.smith21@gmail.com>
2023-07-12 15:41:18 +0100

    Final webpage design
    Afternoon session

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+let viz;
+
 // 1. Create a variable to store the vizContainer
 const vizContainer = document.getElementById("vizContainer");
 
@@ -14,10 +16,66 @@ const url =
 
 // 4. Define function to load the dashboard
 function initViz() {
-  const viz = new tableau.Viz(vizContainer, url, options);
+  viz = new tableau.Viz(vizContainer, url, options);
 }
 
 // 5. Execute function to actually load the dashboard
 document.addEventListener("DOMContentLoaded", initViz);
 
 initViz();
+
+// 6. Create a variable to store the export PDF button
+
+const exportPdfButton = document.getElementById("exportPDF");
+
+// 7. Add event listener for when the button is clicked
+
+exportPdfButton.addEventListener("click", exportPdfFunction);
+
+// 8. Define a function to trigger on button click
+
+function exportPdfFunction() {
+  viz.showExportPDFDialog();
+}
+// 6 - 8 steps again but for a PowerPoint export
+
+const exportPowerPointButton = document.getElementById("exportPowerPoint");
+
+exportPowerPointButton.addEventListener("click", exportPowerPointFunction);
+
+function exportPowerPointFunction() {
+  viz.showExportPowerPointDialog();
+}
+
+// 10. set up filter interactions
+function getRangeValues() {
+  const minValue = document.getElementById("minValue").value;
+  console.log(minValue, maxValue);
+}
+
+// 11. trigger function on filter button click
+const filterButton = document.getElementById("filterButton");
+
+filterButton.addEventListener("click", getRangeValues);
+
+function getRangeValues() {
+  const minValue = document.getElementById("minValue").value;
+  const maxValue = document.getElementById("maxValue").value;
+
+  console.log(minValue, maxValue);
+
+  const workbook = viz.getWorkbook();
+  const activeSheet = workbook.getActiveSheet();
+  const sheets = activeSheet.getWorksheets();
+
+  //inspect the sheets you need to filter
+
+  console.log(sheets);
+  const sheetToFilter = sheets[0];
+
+  // do the actual filtering
+
+  sheetToFilter
+    .applyRangeFilterAsync("SUM(Sales)", { min: minValue, max: maxValue })
+    .then(alert("viz filtered 😘"));
+}
